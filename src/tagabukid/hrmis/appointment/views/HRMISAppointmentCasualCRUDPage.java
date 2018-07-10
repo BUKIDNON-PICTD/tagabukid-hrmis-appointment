9ae1990567e3bd582066ebefa8a683c5122fe9cb
rufdev <aguilarufino@gmail.com>
2018-07-10 08:50:18 +0800

    added sample report for gender sensitivity listing

--- src/tagabukid/hrmis/appointment/views/HRMISAppointmentCasualCRUDPage.java
+++ src/tagabukid/hrmis/appointment/views/HRMISAppointmentCasualCRUDPage.java
@@ -223,7 +223,7 @@ public class HRMISAppointmentCasualCRUDPage extends javax.swing.JPanel {
                 , new Object[]{"nullWhenEmpty", true}
                 , new Object[]{"editable", false}
                 , new Object[]{"textCase", com.rameses.rcp.constant.TextCase.NONE}
-                , new Object[]{"typeHandler", new com.rameses.rcp.common.DecimalColumnHandler("#,##0.00", -1.0, -1.0, false, 2)}
+                , new Object[]{"typeHandler", new com.rameses.rcp.common.DecimalColumnHandler("#,##0.0000", -1.0, -1.0, false, 2)}
             }),
             new com.rameses.rcp.common.Column(new Object[]{
                 new Object[]{"name", "attachment"}
