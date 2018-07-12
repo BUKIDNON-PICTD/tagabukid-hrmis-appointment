2f33fab8d089fc8a85e9f1264452b44b5a0d6d1f
unknown <bylgrad@gmail.com>
2018-07-12 13:45:06 +0800

    Fin Account Management not working

---
+++ src/tagabukid/hrmis/appointment/models/FundManagementListController.groovy
@@ -0,0 +1,38 @@
+package tagabukid.hrmis.appointment.models
+
+import com.rameses.osiris2.common.ExplorerViewController;
+
+public abstract class FundManagementListController extends ExplorerViewController {
+    
+    public abstract String getServiceName();
+    public abstract Object getHrmisfundService();
+    public abstract String getTitle();
+    
+    def params = [:]; 
+    
+//    def types = ['Casual', 'Permanent'];
+    public String getPrefixId() {
+        return "FUND";
+    }
+
+    def selectedItem
+    
+    public String getDefaultFileType() {
+        return "hrmisfund";
+    }
+    
+    public String getContext() {
+        return "hrmisfund";
+    }    
+
+    public void beforeFetchList( Map qry ) { 
+        if ( params ) qry.putAll( params ); 
+    }
+    
+    public void beforeNodes(Map qry){
+
+        if ( params ) qry.putAll( params ); 
+        
+    }
+
+}
