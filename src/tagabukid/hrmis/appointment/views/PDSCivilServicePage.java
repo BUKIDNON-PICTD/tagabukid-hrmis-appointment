9d17edd1b8d9c463921378826d97da4bc50518bc
rufdev <aguilarufino@gmail.com>
2018-07-26 13:46:21 +0800

    finalized civili service eligibility section

--- src/tagabukid/hrmis/appointment/views/PDSCivilServicePage.java
+++ src/tagabukid/hrmis/appointment/views/PDSCivilServicePage.java
@@ -48,7 +48,7 @@ public class PDSCivilServicePage extends javax.swing.JPanel {
         xDataTable1.setColumns(new com.rameses.rcp.common.Column[]{
             new com.rameses.rcp.common.Column(new Object[]{
                 new Object[]{"name", "eligibility"}
-                , new Object[]{"caption", "Career Service/RA1080(Board/Bar) Under Special Laws/CES/CSEE Barangay Eiligibility/Driver's License"}
+                , new Object[]{"caption", "Eligibility"}
                 , new Object[]{"width", 100}
                 , new Object[]{"minWidth", 0}
                 , new Object[]{"maxWidth", 0}
@@ -59,6 +59,19 @@ public class PDSCivilServicePage extends javax.swing.JPanel {
                 , new Object[]{"editableWhen", null}
                 , new Object[]{"textCase", com.rameses.rcp.constant.TextCase.UPPER}
                 , new Object[]{"typeHandler", new com.rameses.rcp.common.LookupColumnHandler("#{item.eligibility.name}", "lookup:tagabukid_hrmis_eligibility")}
+            }),
+            new com.rameses.rcp.common.Column(new Object[]{
+                new Object[]{"name", "eligibility.eligibilitytype.name"}
+                , new Object[]{"caption", "Eligibility Type"}
+                , new Object[]{"width", 100}
+                , new Object[]{"minWidth", 0}
+                , new Object[]{"maxWidth", 0}
+                , new Object[]{"required", false}
+                , new Object[]{"resizable", true}
+                , new Object[]{"nullWhenEmpty", true}
+                , new Object[]{"editable", false}
+                , new Object[]{"textCase", com.rameses.rcp.constant.TextCase.NONE}
+                , new Object[]{"typeHandler", new com.rameses.rcp.common.TextColumnHandler()}
             }),
             new com.rameses.rcp.common.Column(new Object[]{
                 new Object[]{"name", "rating"}
