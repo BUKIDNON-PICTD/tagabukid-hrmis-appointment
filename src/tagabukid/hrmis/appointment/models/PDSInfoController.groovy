3d3e025e9f724a169f42eb09a515a1f1ae5d06f0
rufdev <aguilarufino@gmail.com>
2018-07-30 11:39:10 +0800

    remove Env import

--- src/tagabukid/hrmis/appointment/models/PDSInfoController.groovy
+++ src/tagabukid/hrmis/appointment/models/PDSInfoController.groovy
@@ -2,7 +2,6 @@ import com.rameses.rcp.annotations.*
 import com.rameses.rcp.common.*
 import com.rameses.osiris2.client.*
 import com.rameses.osiris2.common.*
-import com.rameses.annotations.Env
 import com.rameses.common.*;
 import com.rameses.seti2.models.*;
 import com.rameses.util.*;
--- src/tagabukid/hrmis/appointment/models/PDSOtherInfoSectionController.groovy
+++ src/tagabukid/hrmis/appointment/models/PDSOtherInfoSectionController.groovy
@@ -1,7 +1,6 @@
 import com.rameses.rcp.annotations.*
 import com.rameses.rcp.common.*
 import com.rameses.seti2.models.*;
-import com.rameses.annotations.Env
 import com.rameses.osiris2.client.*
 import com.rameses.osiris2.common.*
 import java.rmi.server.*
