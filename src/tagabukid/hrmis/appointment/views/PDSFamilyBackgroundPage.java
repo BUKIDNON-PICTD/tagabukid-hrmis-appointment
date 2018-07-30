323740fd8033fd2c7f51e1b30a327a19899ccd50
rufdev <aguilarufino@gmail.com>
2018-07-30 15:50:47 +0800

    added maiden name to spouse

--- src/tagabukid/hrmis/appointment/views/PDSFamilyBackgroundPage.java
+++ src/tagabukid/hrmis/appointment/views/PDSFamilyBackgroundPage.java
@@ -399,6 +399,19 @@ public class PDSFamilyBackgroundPage extends javax.swing.JPanel {
                 , new Object[]{"typeHandler", new com.rameses.rcp.common.TextColumnHandler()}
             }),
             new com.rameses.rcp.common.Column(new Object[]{
+                new Object[]{"name", "maidenname"}
+                , new Object[]{"caption", "Maiden Name"}
+                , new Object[]{"width", 100}
+                , new Object[]{"minWidth", 0}
+                , new Object[]{"maxWidth", 0}
+                , new Object[]{"required", false}
+                , new Object[]{"resizable", true}
+                , new Object[]{"nullWhenEmpty", true}
+                , new Object[]{"editable", false}
+                , new Object[]{"textCase", com.rameses.rcp.constant.TextCase.NONE}
+                , new Object[]{"typeHandler", new com.rameses.rcp.common.TextColumnHandler()}
+            }),
+            new com.rameses.rcp.common.Column(new Object[]{
                 new Object[]{"name", "nameextension"}
                 , new Object[]{"caption", "Extension Name"}
                 , new Object[]{"width", 20}
