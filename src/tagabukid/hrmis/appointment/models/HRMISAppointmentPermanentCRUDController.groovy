66590726e0ff436329c2d35c63e171cdd8f00b49
rufdev <aguilarufino@gmail.com>
2019-01-30 10:25:32 +0800

    finished CRUD for permanent appointment

--- src/tagabukid/hrmis/appointment/models/HRMISAppointmentPermanentCRUDController.groovy
+++ src/tagabukid/hrmis/appointment/models/HRMISAppointmentPermanentCRUDController.groovy
@@ -14,37 +14,45 @@ class HRMISAppointmentPermanentCRUDController  extends CrudFormModel{
     @Service('DateService')
     def dtSvc
     
-    @Caller
-    def renewcaller
-    
-    
     @Service("PersistenceService")
     def persistenceSvc;
     
-    @Service("TagabukidHRMISAppointmentCasualService")
+    @Service("TagabukidHRMISAppointmentPermanentService")
     def svc
     
     @Service('TagabukidLookupService')
     def tgbkdSvc
-    
+
     def tag
-    def selectedAppointmentItem
     
+    @PropertyChangeListener
+    def listener = [
+        'entity.plantilla' : { 
+               entity.paygradeandstepincrement = entity.plantilla.paygradeandstepincrement
+               calculatewage() 
+               binding.refresh()
+        }
+//        ,
+//        'entity.paygradeandstepincrement' : { 
+//               calculatewage() 
+//        }
+        
+    ]
     boolean isAllowApprove() {
-        return ( mode=='read' && entity.state.toString().matches('DRAFT|CUTOFF') ); 
+        return ( mode=='read' && entity.state.toString().matches('PENDING|CUTOFF') ); 
     }
     
     boolean isallowPreviewAssumptionOfDuty() {
         return ( mode=='read'); 
     }
     
-    boolean isAllowRenew() {
-        def currdate = new java.sql.Date(dtSvc.getServerDate().time);
-        def datediff = entity.effectiveuntil.time - currdate.time
-        def range = 0..60
-//        return (mode=='read' && entity.state=='APPROVED' && range.contains((datediff / (60*60*24*1000)) as int)); 
-        return (mode=='read' && entity.state=='APPROVED' && ((datediff / (60*60*24*1000)) as int) < 0); 
-    }
+//    boolean isAllowRenew() {
+//        def currdate = new java.sql.Date(dtSvc.getServerDate().time);
+//        def datediff = entity.effectiveuntil.time - currdate.time
+//        def range = 0..60
+////        return (mode=='read' && entity.state=='APPROVED' && range.contains((datediff / (60*60*24*1000)) as int)); 
+//        return (mode=='read' && entity.state=='APPROVED' && ((datediff / (60*60*24*1000)) as int) < 0); 
+//    }
     
     boolean isAllowCutoff() {
         return (mode=='read' && entity.state=='APPROVED'); 
@@ -64,106 +72,46 @@ class HRMISAppointmentPermanentCRUDController  extends CrudFormModel{
     }
 
     public void beforeSave(o){
-        entity.appointmentitems.each{
-            def pds = persistenceSvc.read([_schemaname:'hrmis_pds', objid:it.pds.objid])
-            it.personnel = pds.person
+//        println entity
+        if(o == 'update'){
+            entity.recordlog.dateupdated = dtSvc.getServerDate();
+            entity.recordlog.lastupdatedbyuser = OsirisContext.env.FULLNAME;
+            entity.recordlog.lastupdatedbyuserid = OsirisContext.env.USERID;
         }
+        entity.pds._schemaname = 'hrmis_pds'
+        persistenceSvc.update(entity.pds)
     }
-
+    public void afterSave(){
+        entity.org = entity.plantilla.org
+    }
     public void afterCreate(){
-        tag = invoker?.properties?.tag;
-        if(tag=='renew'){
-            entity.putAll(svc.initRenew(renewcaller.entity))
-//            println entity.currentsalarystep
-//            println mode
-        }else{
-            entity = svc.initCreate();
-        }
-      
+        entity = svc.initCreate();
     }
 
-//    public void afterOpen(){
+//    public void afterEdit(){
 //        println entity
 //    }
-
-    def suggestGroupName = [
-        fetchList: { o->
-            return svc.getList(o).appointmentgroupname;
-        }
-//        ,
-//        onselect:{ o->
-//            entity.appointmentitems = svc.getAppointmentItemsByGroup(o)
-//
+    
+//    def suggestGroupName = [
+//        fetchList: { o->
+//            return svc.getList(o).appointmentgroupname;
 //        }
-    ] as SuggestModel;
+////        ,
+////        onselect:{ o->
+////            entity.appointmentitems = svc.getAppointmentItemsByGroup(o)
+////
+////        }
+//    ] as SuggestModel;
 
-    def appointmentitemListHandler = [
-        fetchList: { 
-            entity.appointmentitems.each{
-                def pds = persistenceSvc.read([_schemaname:'hrmis_pds', objid:it.pds.objid])
-//                it.personnel = tgbkdSvc.getEntityByObjid([entityid:it.personnel.objid]);
-                it.pds = pds
-                it.plantilla = tgbkdSvc.findPlantillaById([plantillaid:it.plantilla.objid]);
-            }
-            return entity.appointmentitems 
-        
-        },
-        createItem : {
-            return[
-                objid : 'ACI' + new java.rmi.server.UID() +"-"+ dtSvc.getServerDate().year,
-            ]
-        },
-        onRemoveItem : {
-            if (entity.state != 'DRAFT'){
-                MsgBox.alert("Delete is not allowed.")
-                return false
-            }else if (MsgBox.confirm('Delete item?')){                
-                entity.appointmentitems.remove(it)
-                persistenceSvc.removeEntity([_schemaname:'hrmis_appointmentcasualitems',objid:it.objid])
-                appointmentitemListHandler?.load();
-                return true;
-            }
-            return false;
-        },
-        onColumnUpdate: { o,col-> 
-            if(col == 'plantilla'){
-                o.salaryscheduleitem  = svc.getDailyWageByTranch(entity.currentsalarystep,o.plantilla);
-                o.monthlywage = o.salaryscheduleitem.amount
-                o.dailywage = o.salaryscheduleitem.amount / 22
-            }
-            
-            if(col == 'cutoffdate'){
-                SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
-                java.util.Date date = sdf1.parse(o.cutoffdate);
-                java.sql.Date cutoffdate = new java.sql.Date(date.getTime()); 
-                if (entity.effectivefrom >= cutoffdate || cutoffdate >= entity.effectiveuntil){
-                    throw new Exception("Cut-off date must be between Effective From and Effective Until.");
-                }
-            }
-            
-        },
-        onCommitItem:{ x ->
-//            println x
-            if (x.cutoffdate && !x.cutoffreason.objid){
-                throw new Exception("Cut-off Reason is required.");
-            }
-        },
-        onAddItem : {
-//            it.plantilla.Id = it.plantilla.Id.toString()
-//            it.monthlywage = it.dailywage * 22
-            entity.appointmentitems.add(it);
-        },
-        validate:{li->
-            def item=li.item;
-            //checkDuplicateIPCR(selectedDPCR.ipcrlist,item);
-        }
-    ] as EditorListModel
 
     def signatoryItemHandler = [
         fetchList: { 
-            if(entity.signatorygroup?.objid)
-            entity.signatorygroup = persistenceSvc.read( [_schemaname:'hrmis_appointment_signatorygrouping', objid:entity.signatorygroup.objid] );
-            return entity.signatorygroup?.signatoryGroupItems 
+            if(entity?.signatorygroup?.objid){
+                entity.signatorygroup = persistenceSvc.read( [_schemaname:'hrmis_appointment_signatorygrouping', objid:entity.signatorygroup.objid] );
+                return entity.signatorygroup?.signatoryGroupItems 
+            }
+            return []
+           
         },
         onRemoveItem : {
             if (MsgBox.confirm('Delete item?')){                
@@ -182,7 +130,7 @@ class HRMISAppointmentPermanentCRUDController  extends CrudFormModel{
     void approve() { 
         if ( MsgBox.confirm('You are about to approve this document. Proceed?')) { 
             getPersistenceService().update([ 
-                    _schemaname: 'hrmis_appointmentcasual', 
+                    _schemaname: 'hrmis_appointmentpermanent', 
                     objid : entity.objid, 
                     state : 'APPROVED' 
                 ]); 
@@ -193,7 +141,7 @@ class HRMISAppointmentPermanentCRUDController  extends CrudFormModel{
      void cutoff() { 
         if ( MsgBox.confirm('You are about to edit this document. Proceed?')) { 
             getPersistenceService().update([ 
-                    _schemaname: 'hrmis_appointmentcasual', 
+                    _schemaname: 'hrmis_appointmentpermanent', 
                     objid : entity.objid, 
                     state : 'CUTOFF' 
                 ]); 
@@ -209,12 +157,8 @@ class HRMISAppointmentPermanentCRUDController  extends CrudFormModel{
 //            ]);
 //    }
     void calculatewage(){
-        entity.appointmentitems.each{
-            it.salaryscheduleitem  = svc.getDailyWageByTranch(entity.currentsalarystep,it.plantilla);
-            it.monthlywage = it.salaryscheduleitem.amount
-            it.dailywage = it.salaryscheduleitem.amount / 22
-        }
-        appointmentitemListHandler.reload();
+        def salaryscheduleitem  = svc.getDailyWageByTranch(entity.plantilla);
+        entity.compensation = salaryscheduleitem.amount
     }
     // def getPersonnelLookupHandler(){
     //     return Inv.lookupOpener('lookup:individualwide',[
@@ -224,16 +168,16 @@ class HRMISAppointmentPermanentCRUDController  extends CrudFormModel{
     //         ]);
     // }
 
-    // void validatecurrentappointment(o){
-    //     if(!entity.effectivefrom){
-    //         throw new Exception("Effective From is required.");
-    //     }
-    //     if(svc.findPersonnelHasActiveAppointment(o,entity.effectivefrom)){
-    //         throw new Exception("Effective From is required.");
-    //     }
-    // }
+     void validatecurrentappointment(o){
+         if(!entity.effectivefrom){
+             throw new Exception("Effective From is required.");
+         }
+         if(svc.findPersonnelHasActiveAppointment(o,entity.effectivefrom)){
+             throw new Exception("Effective From is required.");
+         }
+     }
    
-    def renew(){
-        return InvokerUtil.lookupOpener('hrmis_appointmentcasual:renew:create')
-    }
+//    def renew(){
+//        return InvokerUtil.lookupOpener('hrmis_appointmentcasual:renew:create')
+//    }
 }  
